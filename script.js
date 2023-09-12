4c5ffeb9dfdfc2f8f6ad05db66286ab3a958c422
tnajp <144614687+tnajp@users.noreply.github.com>
2023-09-12 15:19:51 +0430

    Created the CSS and JS files

---
+++ script.js
@@ -0,0 +1,84 @@
+let currentTime = new Date();
+function formatDate(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[currentTime.getDay()];
+  let hour = currentTime.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  let minutes = currentTime.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day}  ${hour}:${minutes}`;
+}
+let dayntime = document.querySelector("#current-time");
+dayntime.innerHTML = formatDate(currentTime);
+function changeToCelsius(event) {
+  event.preventDefault();
+  let modifiedTemp = document.querySelector("#temperature");
+  modifiedTemp.innerHTML = 19;
+}
+let celsius = document.querySelector("#celsius-link");
+celsius.addEventListener("click", changeToCelsius);
+
+function changeToFahrenheit(event) {
+  event.preventDefault();
+  let modifiedTemp = document.querySelector("#temperature");
+  let temperature = modifiedTemp.innerHTML;
+  temperature = Number(temperature);
+  modifiedTemp.innerHTML = Math.round((temperature * 9) / 5 + 32);
+}
+let fahrenheit = document.querySelector("#fahrenheit-link");
+fahrenheit.addEventListener("click", changeToFahrenheit);
+//new
+function showWeather(response) {
+  console.log(response.data);
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#country").innerHTML = response.data.sys.country;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#wind-speed").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+}
+///
+function searchCity(city) {
+  let apiKey = "7784a4cd4aa2e0c25ead7bd96d585b8a";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(showWeather);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+let searchForm = document.querySelector("#city-search");
+searchForm.addEventListener("submit", handleSubmit);
+
+function searchLocation(position) {
+  let apiKey = "7784a4cd4aa2e0c25ead7bd96d585b8a";
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(showWeather);
+}
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+searchCity("New York");
