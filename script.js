8260df57d9bf9a98ed661be605b4e19f884d4443
tnajp <144614687+tnajp@users.noreply.github.com>
2023-09-14 20:09:34 +0430

    Added getForecast func & the api for forecast (one call)

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function formatDate(timestamp, timezone = null) {
   }
   return `${day}  ${hour}:${minutes}`;
 }
-function displayForecast(params) {
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
   let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue", "Wed"];
   let forecastHTML = `<div class="row">`;
@@ -72,6 +72,11 @@ let celsius = document.querySelector("#celsius-link");
 celsius.addEventListener("click", changeToCelsius);
 let celsiusTemperature = null;
 //new
+function getForecast(coordinates) {
+  let apiKey = "7784a4cd4aa2e0c25ead7bd96d585b8a";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
 function showWeather(response) {
   console.log(response.data);
   document.querySelector("#city").innerHTML = response.data.name;
@@ -95,6 +100,7 @@ function showWeather(response) {
     response.data.dt * 1000,
     response.data.timezone
   );
+  getForecast(response.data.coord);
 }
 ///
 function searchCity(city) {
@@ -124,4 +130,3 @@ function getCurrentLocation(event) {
 let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
 searchCity("New York");
-displayForecast();
